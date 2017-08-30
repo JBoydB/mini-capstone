392ef8905b6873cae63477c7ee3f8910c4fae10a
JBoydB <Jaronb91@yahoo.com>
2017-08-29 20:45:45 -0700

    starting on webpages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,27 @@
 class ProductsController < ApplicationController
+  
+  def all_products
+    @all_products = Product.all
+    render "allproducts.html.erb"
+  end
+
+  def tv
+    @tv = Product.find_by(id: 1)
+    render "tv.html.erb"
+  end
+
+  def console
+    @console = Product.find_by(id: 2)
+    render "console.html.erb"
+  end
+
+  def remote
+    @remote = Product.find_by(id: 3)
+    render "remote.html.erb"
+  end
+
+  def paper
+    @paper = Product.find_by(id: 4)
+    render "paper.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/allproducts" => "products#all_products"
+  get "/tv" => "products#tv"
+  get "/console" => "products#console"
+  get "/remote" => "products#remote"
+  get "/paper" => "products#paper"
 end
